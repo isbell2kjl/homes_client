86ac8b1bbcc2b90060a4a1ebb69fd8c4b39f7931
Keith Isbell <keith.isbell@Keith-Isbell-Lenovo>
2024-10-10 21:43:21 -0700

    upgraded angular from 17 to 18

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,7 +9,7 @@ import { UserComponent } from './components/user/user.component';
 import { PostNewComponent } from './components/post-new/post-new.component';
 import { PostListComponent } from './components/post-list/post-list.component';
 import { PostEditComponent } from './components/post-edit/post-edit.component';
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavigationPageComponent } from './navigation-page/navigation-page.component';
@@ -40,50 +40,42 @@ import { UserService } from './services/user.service';
 import { WebmasterComponent } from './components/webmaster/webmaster.component';
 
 
-@NgModule({
-  declarations: [
-    AppComponent,
-    SignUpComponent,
-    SignInComponent,
-    UserComponent,
-    PostNewComponent,
-    PostListComponent,
-    PostEditComponent,
-    NavigationPageComponent,
-    UserEditComponent,
-    UserSearchComponent,
-    CommentNewComponent,
-    CommentEditComponent,
-    HomeComponent,
-    PostActiveComponent,
-    ContactComponent,
-    ForgotPasswordComponent,
-    ResetPasswordComponent,
-    WebmasterComponent,
-  ],
-  imports: [
-
-    AppRoutingModule,
-    HttpClientModule,
-    FormsModule,
-    BrowserAnimationsModule,
-    LayoutModule,
-    MatToolbarModule,
-    MatButtonModule,
-    MatSidenavModule,
-    MatIconModule,
-    MatListModule,
-    MatFormFieldModule,
-    MatSelectModule,
-    MatInputModule,
-    MatCheckboxModule,
-    MatSlideToggleModule,
-    MatButtonToggleModule,
-    MatProgressSpinnerModule,
-    ReactiveFormsModule,
-    RecaptchaModule,
-  ],
-  providers: [{ provide: APP_INITIALIZER, useFactory: appInitializer, multi: true, deps: [UserService] },],
-  bootstrap: [AppComponent]
-})
+@NgModule({ declarations: [
+        AppComponent,
+        SignUpComponent,
+        SignInComponent,
+        UserComponent,
+        PostNewComponent,
+        PostListComponent,
+        PostEditComponent,
+        NavigationPageComponent,
+        UserEditComponent,
+        UserSearchComponent,
+        CommentNewComponent,
+        CommentEditComponent,
+        HomeComponent,
+        PostActiveComponent,
+        ContactComponent,
+        ForgotPasswordComponent,
+        ResetPasswordComponent,
+        WebmasterComponent,
+    ],
+    bootstrap: [AppComponent], imports: [AppRoutingModule,
+        FormsModule,
+        BrowserAnimationsModule,
+        LayoutModule,
+        MatToolbarModule,
+        MatButtonModule,
+        MatSidenavModule,
+        MatIconModule,
+        MatListModule,
+        MatFormFieldModule,
+        MatSelectModule,
+        MatInputModule,
+        MatCheckboxModule,
+        MatSlideToggleModule,
+        MatButtonToggleModule,
+        MatProgressSpinnerModule,
+        ReactiveFormsModule,
+        RecaptchaModule], providers: [{ provide: APP_INITIALIZER, useFactory: appInitializer, multi: true, deps: [UserService] }, provideHttpClient(withInterceptorsFromDi()),] })
 export class AppModule { }
