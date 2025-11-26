0cd81a8c3206c644c08407e429e08c9904a80f78
Keith Isbell <keith.isbell@Keith-Isbell-Lenovo>
2025-11-25 18:52:44 -0800

    changed baseURL to relative path for www or non-www

--- src/app/helpers/constants.ts
+++ src/app/helpers/constants.ts
@@ -1,7 +1,12 @@
 // export const baseURL: string = 'https://localhost:7279/api';
 // export const baseURL: string = 'https://myproperties.ddns.net/api';
-export const baseURL: string = 'https://group.myadvantageproperties.com/api';
+//export const baseURL: string = 'https://myadvantageproperties.com/api';
 
+//Use relative path since Angular and the API are served from the same Nginx server.
+//Angular will automatically call the backend on the same domain the app is loaded from (www or non-www).
+export const baseURL: string = '/api';
+
+//webSite=true if used as public website or false if used for internal tracking only.
 export const webSite: boolean = true;
 
 export const StrongPasswordRegx: RegExp =  
